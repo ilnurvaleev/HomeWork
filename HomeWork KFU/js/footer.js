9a3415110500018b251185dfeda24b7dbad7234a
Ilnur Valeev <39234087+ilnurvaleev@users.noreply.github.com>
2022-05-06 23:15:02 +0300

    Create footer.js

---
+++ HomeWork KFU/js/footer.js
@@ -0,0 +1,8 @@
+document.writeln(`
+<hr class="featurette-divider">
+<footer class="container">
+  <p class="float-right"><a href="#">Back to top</a></p>
+  <p>© 2022-2022 Ilnur. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+</footer>
+`
+);
